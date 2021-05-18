26bbb9f1cf337ccd6d193541e0c1aeb0ff0c64ac
redHurt96 <62645756+redHurt96@users.noreply.github.com>
2021-05-18 10:14:50 +0300

    Clear example PlayerData

--- Between/Assets/Scripts/Saving/PlayerData.cs
+++ Between/Assets/Scripts/Saving/PlayerData.cs
@@ -8,16 +8,10 @@ namespace Between.Saving
     [Serializable]
     public class PlayerData
     {
-        public bool SomeBoolValue;
-        public int SomeIntValue;
-        public string SomeStringValue;
+
 
         public PlayerData CreateDefault()
         {
-            SomeBoolValue = false;
-            SomeIntValue = 123;
-            SomeStringValue = "Default";
-
             return this;
         }
     }
