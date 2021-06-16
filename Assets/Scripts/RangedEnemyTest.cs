e34615d674cc82beb35eef61b3d5acd46c2f816d
CursedSorbet <asocialshark@gmail.com>
2021-06-16 15:43:49 +0300

    Добавил редж врага без механики создания проджектайла

---
+++ Assets/Scripts/RangedEnemyTest.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedEnemyTest : MonoBehaviour
+{
+    private bool isAgred;
+    public float agroRange;
+    public Transform target;
+
+    public float attackDelay;
+    private float attackCD;
+    public GameObject arrow;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, target.transform.position) < agroRange)
+        {
+            if (!Physics.Raycast(transform.position, (target.position - transform.position).normalized, Mathf.Infinity, 1 << 3))
+            {
+                isAgred = true;
+            }
+        }
+        else
+            isAgred = false;
+
+        if (isAgred)
+        {
+            Shooting();
+        }
+
+        if (attackCD > 0)
+        {
+            attackCD -= 1*Time.deltaTime ;
+        }
+    }
+
+    void Shooting()
+    {
+        if (attackCD <= 0)
+        {
+
+            attackCD = attackDelay;
+        }
+    }
+}
