c51e066db9b247cd4f303e71da193a671b05a3a6
redHurt96 <62645756+redHurt96@users.noreply.github.com>
2021-08-05 17:55:51 +0300

    Update MeleeAttackState.cs

--- Assets/_Between/Scripts/Enemy/Skeletons/States/MeleeAttackState.cs
+++ Assets/_Between/Scripts/Enemy/Skeletons/States/MeleeAttackState.cs
@@ -1,5 +1,6 @@
 using Between.Animations;
 using Between.Damage;
+using Between.Sounds;
 using Between.StateMachine;
 using UnityEngine;
 
@@ -38,7 +39,7 @@ namespace Between.Enemies.Skeletons
         private void PlayAttackSound()
         {
             if (_attack != null)
-                AudioSource.PlayClipAtPoint(_attack, _animator.transform.position);
+                AudioSource.PlayClipAtPoint(_attack, _animator.transform.position, MainVolume.Value);
         }
 
         private void ApplyDamageToPlayer()
@@ -58,7 +59,7 @@ namespace Between.Enemies.Skeletons
         private void PlayHitSound()
         {
             if (_hit != null)
-                AudioSource.PlayClipAtPoint(_hit, _animator.transform.position);
+                AudioSource.PlayClipAtPoint(_hit, _animator.transform.position, MainVolume.Value);
         }
     }
 }
