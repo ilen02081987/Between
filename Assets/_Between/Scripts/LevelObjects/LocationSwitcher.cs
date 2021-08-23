41ed080e5f863f94a0bbb3b3930396ab5655352c
redHurt96 <62645756+redHurt96@users.noreply.github.com>
2021-08-23 17:58:01 +0300

    fix portal

--- Assets/_Between/Scripts/LevelObjects/LocationSwitcher.cs
+++ Assets/_Between/Scripts/LevelObjects/LocationSwitcher.cs
@@ -1,7 +1,18 @@
+using Knife.Portal;
+using System;
+using UnityEngine;
+
 namespace Between.LevelObjects
 {
     public class LocationSwitcher : InteractableObject
     {
+        [SerializeField] private PortalTransition _portalVfx;
+
+        private void Start()
+        {
+            _portalVfx.OpenPortal();
+        }
+
         protected override void PerformOnInteract() { }
     }
 }
